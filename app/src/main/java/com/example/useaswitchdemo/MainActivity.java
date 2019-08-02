958b426f8feec33addfcc0ff842af703d4776f6d
AcHumphrey <AcHumphrey@example.com>
2019-08-02 10:34:03 +0100

    Initial commit

---
+++ app/src/main/java/com/example/useaswitchdemo/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.useaswitchdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    View btn_show_hide;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Switch onOffSwitch = (Switch) findViewById(R.id.on_off_switch);
+        btn_show_hide   = findViewById(R.id.btn_show_hide);
+
+        onOffSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked){
+
+                  btn_show_hide.setVisibility(View.VISIBLE);
+
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "The Button is Visible",
+                            Toast.LENGTH_LONG);
+                    toast.show();
+                }else{
+
+                    btn_show_hide.setVisibility(View.INVISIBLE);
+
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "The Button is Invisible",
+                            Toast.LENGTH_LONG);
+                    toast.show();
+                }
+
+            }
+
+        });
+    }
+}
